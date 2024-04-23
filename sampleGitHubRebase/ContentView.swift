726974e51413bf21711c1937c18ff0f505640a2c
priteeshh@gmail.com <priteeshh@gmail.com>
2024-04-23 22:57:57 +0400

    Initial Commit

---
+++ sampleGitHubRebase/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  sampleGitHubRebase
+//
+//  Created by Preeteesh Remalli on 23/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sampleGitHubRebase/sampleGitHubRebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleGitHubRebaseApp.swift
+//  sampleGitHubRebase
+//
+//  Created by Preeteesh Remalli on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct sampleGitHubRebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
